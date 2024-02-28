0f294b9fc8fa08a3f4019bba30e50c3f239dd663
DANIELAZAPATA0724 <dzapata0724@gmail.com>
2024-02-28 15:44:38 +0100

    update

--- LoginCrud-Express/backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-// import express from "express";
-// import { Login, Register } from "../controllers/authController.js";
-
-// const router = express.Router();
-// router.post("/register", Register);
-// router.post("/login", Login);
-
-// export default router;
